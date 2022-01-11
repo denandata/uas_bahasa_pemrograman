e9243392022b5df1c7cfe52f6e9a4957608c9a79
Gede Krishna <94939856+gedekrizhna@users.noreply.github.com>
2022-01-11 11:19:54 +0700

    Update eksepsi_daftar_kabupaten.py

--- eksepsi_daftar_kabupaten.py
+++ eksepsi_daftar_kabupaten.py
@@ -1,19 +1,13 @@
-import sys
-def main():
-    print("PROGRAM  PEMBAGIAN BILANGAN")
-
-    a = float(input("Masukkan a: "))
-    b = float(input("Masukkan b: "))
-
-    try:
-        hasil = a/b
-    except ZeroDivisionError:
-        print("\nERROR: Nilai b tidak boleh nol")
-        sys.exit(1)
-
-    print("\na : ", a)
-    print("b : ", b)
-    print("a / b = ", hasil)
-
-if __name__ == "__main__":
-    main()
+import time
+    f= open('coba.txt')
+    while True:
+        baris = f.redline ()
+        if len(baris) == 0:
+                #EOF
+                break
+              print baris,
+              time.sleep(2)     # delay 2 detik
+ except KeyboardInterrupt :
+    print '\nAnda membatalkan operasi'
+    f.close ()
+    print '\nfile ditutup.'
